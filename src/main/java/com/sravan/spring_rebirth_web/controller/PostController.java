61a3bc436ae84ea996105e3f9b7754c0a412e499
sravan-kumarc <sravankumarchagalamarri@gmail.com>
2026-05-19 14:41:40 +0530

    Implemented POST APIs with RequestBody and Employee model

---
+++ src/main/java/com/sravan/spring_rebirth_web/controller/PostController.java
@@ -0,0 +1,39 @@
+package com.sravan.spring_rebirth_web.controller;
+
+import com.sravan.spring_rebirth_web.model.Employee;
+import com.sravan.spring_rebirth_web.model.J3User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+
+    @PostMapping("/test")
+    public String testPost() {
+
+        return "POST API Working Successfully!";
+    }
+    @PostMapping("/user")
+    public String createUser(@RequestBody J3User user) {
+
+        return """
+                <h2>User Created Successfully</h2>
+
+                Name: %s <br>
+                Role: %s
+                """.formatted(user.getName(), user.getRole());
+    }
+
+    @PostMapping("/employee")
+    public String insertUser(@RequestBody Employee employee){
+        return """
+               <h2>Member Details</h2>
+               
+               Name: %s,
+               Company: %s,
+               Designation: %s
+           
+               """.formatted(employee.getName(), employee.getCompany(), employee.getDesignation());
+    }
+}
---
+++ src/main/java/com/sravan/spring_rebirth_web/model/J3User.java
@@ -0,0 +1,25 @@
+package com.sravan.spring_rebirth_web.model;
+
+public class J3User {
+    private String name;
+    private String role;
+
+    public J3User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
